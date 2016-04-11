17a3798920d3d2f0c110a300e3f1348ac9dc505b
OoogleBoogle <simattfield@gmail.com>
2016-04-11 11:35:10 -0700

    sudo code for js laid out

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	// start - ryu-still
+	// on hover 
+	  // ryu-ready.show()
+	  // ryu-still.hide()
+	// on click
+	  // ryu -ready .hide()
+	  // image - swap to ryu-thowing
+	  // hadouken .show().animate(css: left);
+	// on keypress 'X'
+	  // ryu ready .hide()
+	  // ryu-cool .show()
+});
+
